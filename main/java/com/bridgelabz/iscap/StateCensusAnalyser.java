e87f6c20554d831b94bd086b16ce40f2e1116558
Saravana20-S <iamsaravanan1347@gmail.com>
2026-07-18 10:46:12 +0530

    first commit

---
+++ main/java/com/bridgelabz/iscap/StateCensusAnalyser.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.iscap;
+
+public class StateCensusAnalyser {
+}
---
+++ main/java/com/bridgelabz/iscap/Welcome.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.iscap;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Project setup");
+    }
+}
